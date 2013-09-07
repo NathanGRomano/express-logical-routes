8ba5e793984b7ff49339ec3515ece68ce3bf2dc2
Nathan <nathan.g.romano@gmail.com>
2013-09-07 01:11:37 +0000

    wrote a test for or() given two true values

---
+++ test/or.js
@@ -0,0 +1,33 @@
+var assert = require('assert')
+	, request = require('supertest')
+	, async = require('async')
+	, express = require('express')
+	, or = require('../.').or 
+
+
+describe('the or() method', function () {
+
+	describe('when invoked with middleware isTrue, and isTrue', function () {
+
+		app = express();
+		app.get('/test', or(isTrue, isTrue), done );
+
+		it('then should invoke our last handler with the value of true', function (done) {
+
+			request(app)
+				.get('/test')
+				.end(function (err, res) {
+					assert.equal(res.body.ok, true);
+					done();
+				});
+
+		});
+
+	});
+
+});
+
+function isTrue (req, res, next) {
+	next(true);
+}
+
